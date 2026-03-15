0a1b5ee79cbd4aaac2f5475cbde2ea669872accb
Matheus Marciano <mm.leitea@outlook.com>
2026-03-15 19:57:56 +0000

    chore: add demo frontend project

---
+++ src/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="(demo, i) in demoList" :key="i">
+      Demo {{ i }}: {{ demo.text }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import axios from 'axios'
+  import { onMounted, ref } from 'vue'
+  import { apiUrl } from '@/utils/api'
+  type Demo = {
+    text: string
+  }
+
+  const demoList = ref<Demo[]>([])
+  onMounted(async () => {
+    console.log(apiUrl('/demo'))
+    const response = await axios.get(apiUrl('/demo/'))
+    demoList.value = response.data
+  })
+</script>
